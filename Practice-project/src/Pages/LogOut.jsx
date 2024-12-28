54bc45556b4390d748da84d2d70dcf4af6d35d23
mahamudul hasan seber <sebermh@gmail.com>
2024-12-28 14:45:43 +0600

    Milestone-11

---
+++ Practice-project/src/Pages/LogOut.jsx
@@ -0,0 +1,39 @@
+import Lottie from "lottie-react";
+import logout from "../../src/assets/lottie/Register.json"
+
+const LogOut = () => {
+    return (
+        <div className="hero bg-base-200 min-h-screen">
+            <div className="hero-content flex-col lg:flex-row-reverse">
+                <div className="text-center lg:text-left w-96">
+                    <Lottie animationData={logout}></Lottie>
+                </div>
+                <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+                    <h1 className="text-3xl font-bold">LogOut</h1>
+                    <form className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="email" placeholder="email" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="password" placeholder="password" className="input input-bordered" required />
+                            <label className="label">
+                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                            </label>
+                        </div>
+                        <div className="form-control mt-6">
+                            <button className="btn btn-primary">LogOut</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default LogOut;
